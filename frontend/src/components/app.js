fe7c7be931eda0f3a9fa6417a97ecdb0f56eba85
Daniel Lin <david82213@hotmail.com>
2016-12-04 19:54:27 -0800

    added tachyon package for later css; begin to implement header of frontend

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Header from './header';
 
 export default class App extends Component {
   render() {
     return (
-      <div data-editable data-name="title">
-        <h1></h1>
-        <p>React simple starter</p>
+      <div>
+        <Header />
+        <div data-editable data-name="title">
+          <p>React simple starter</p>
+        </div>
       </div>
     );
   }
