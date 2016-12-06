0aef0c1c217bddcafb7e2234563dcb1172f5db13
Daniel Lin <david82213@hotmail.com>
2016-12-05 23:14:37 -0800

    added header template to the app

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -3,14 +3,33 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
 class Header extends Component{
+  renderLinks(){
+    // if user is authenticated
+    if (this.props.authenticated) {
+      return
+        <li className="nav-item">
+          <Link className="nav-link" to="/signout">Sign Out</Link>
+        </li>
+    }
+    else{
+      return [
+        // since its static, fine to assign key as plain integer
+        <li className="nav-item" key={1}>
+          <Link className="nav-link" to="/signin">Sign In</Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link className="nav-link" to="/signup">Sign Up</Link>
+        </li>
+      ];
+    }
+  }
+
   render(){
     return (
       <nav className="navbar navbar-light">
         <Link to="/" className="navbar-brand">Easy Site Builder</Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item">
-            Sign in
-          </li>
+          {this.renderLinks()}
         </ul>
       </nav>
     );
